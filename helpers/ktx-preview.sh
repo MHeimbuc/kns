174440884569a2f4d2b88f3008376fe0f4828cb9
Markus Heimbuchner <MHeimbuc@users.noreply.github.com>
2018-10-10 15:40:30 +0200

    Update ktx-preview.sh

--- helpers/ktx-preview.sh
+++ helpers/ktx-preview.sh
@@ -5,12 +5,12 @@ set -eu
 
 selected=$1
 
-context=$(kubectl config view -o jsonpath="{range .contexts[?(@.name == '${selected}')].context}{@.cluster};{@.user};{@.namespace}{end}")
+context=$(kubectl config view -o jsonpath='{range .contexts[?(@.name == "${selected}")].context}{@.cluster};{@.user};{@.namespace}{end}')
 cluster=$(echo "${context}" | cut -d ';' -f 1)
 user=$(echo "${context}" | cut -d ';' -f 2)
 namespace=$(echo "${context}" | cut -d ';' -f 3)
 
-server=$(kubectl config view -o jsonpath="{.clusters[?(@.name == '${cluster}')].cluster.server}")
+server=$(kubectl config view -o jsonpath='{.clusters[?(@.name == "${cluster}")].cluster.server}')
 
 echo "=== ${selected} ==="
 echo
